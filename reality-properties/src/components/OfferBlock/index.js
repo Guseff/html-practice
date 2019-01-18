237cdf86254e6b46d39b45f1f3e0ceb1668c36dd
Guseff <vasily.guseff@gmail.com>
2019-01-18 23:23:20 +0300

    Hot Offer Section Apdated

--- reality-properties/src/components/OfferBlock/index.js
+++ reality-properties/src/components/OfferBlock/index.js
@@ -6,23 +6,26 @@ class OfferBlock extends Component {
   render() {
     return (
       <div className="offer-block">
-        <div className="home-title gray">
+        <div className="offer-label-sale">
+          FOR SALE
+        </div>
+        <div className="offer-title--black">
           Villa With Pool For Sale
         </div>
-        <div className="offer-price blue">
+        <div className="offer-price--blue">
           $225,000
         </div>
-        <div className="offer-address gray">
+        <div className="offer-address--gray">
           263 S Rexford Dr, Beverly Hills, CA 90212
         </div>
         <div className="offer-option-box">
-          <div className="offer-option-beds option">
+          <div className="offer-option--beds option">
             4 Beds
           </div>
-          <div className="offer-option-bath option">
+          <div className="offer-option--bath option">
             2 Baths
           </div>
-          <div className="offer-option-sqft option">
+          <div className="offer-option--sqft option">
             980 SqFt
           </div>
         </div>
